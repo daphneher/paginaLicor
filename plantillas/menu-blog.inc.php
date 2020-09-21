9173f8aeb506a3c0758907544c11e574cd0a7231
daphneher <68396362+daphneher@users.noreply.github.com>
2020-09-21 20:25:27 -0300

    arreglo blog para celular

---
+++ plantillas/menu-blog.inc.php
@@ -0,0 +1,21 @@
+<div id="myNav" class="overlay">
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+    <div class="overlay-content">
+        <a href="<?php echo SERVIDOR ?>">Inicio</a>
+        <a class="nav-link  dropdown-toggle" href="#" data-toggle="dropdown">  Categorías  </a>
+            <ul id="submenu" class="dropdown-menu ">
+                <li><a id="btnSubmenu" class="dropdown-item" href="#"> Accesorios </a></li>
+                <li><a id="btnSubmenu" class="dropdown-item" href="#"> Aprender de licores </a></li>
+                <li><a id="btnSubmenu" class="dropdown-item" href="#" > Cocteles </a></li>
+                <li><a id="btnSubmenu" class="dropdown-item" href="#"> Maridajes </a></li>
+                <li><a id="btnSubmenu" class="dropdown-item" href="#"> Momentos para compartir </a></li>
+                <li><a id="btnSubmenu" class="dropdown-item" href="#"> Expertos dicen </a></li>
+            </ul>
+        <a href="<?php echo SERVIDOR; ?>#blog">Blog</a>
+
+    </div>
+</div>
+
+<div id="menu" >
+    <span id="btn-menu" class="btnMenu" onclick="openNav()">&#9776; Menú</span>
+</div>
