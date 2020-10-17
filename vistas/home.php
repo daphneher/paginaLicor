29ed070e36be397daf7e21749cb0d78a9a008c0c
daphneher <68396362+daphneher@users.noreply.github.com>
2020-10-17 00:36:27 -0300

    cambios de estilos

--- vistas/home.php
+++ vistas/home.php
@@ -8,10 +8,8 @@ include_once "plantillas/encabezado.inc.php";
 ?>
 <div class="container-fluid">
     <div class="row">
-        <div class="col-md-3 col-sm-3">
-            <img id ="logo" class="img-responsive "  src="<?php echo RUTA_IMG ?>logo.png">
-        </div>
-        <div class="col-md-9 col-sm-9">
+        
+        <div class="col-md-12 col-sm-12">
             <!-- Banner -->
             <div class="slideshow-container" id="banner">
                 <?php                
@@ -146,22 +144,7 @@ include_once "plantillas/encabezado.inc.php";
 <!-- Fin Nosotros -->
 
 
-<div class="container" id="contacto" >
-    <h2>Contacto</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>            
-</div>
-<br>
+
 
 <!-- BLOG -->
 <div class="container-fluid" id="blog" style="background-color:#470f11; color:white;">
@@ -198,7 +181,7 @@ include_once "plantillas/documento-redes-sociales.inc.php";
     </div>
 </div>
 <div id="menu" >
-    <span id="btn-menu" class="sticky" onclick="openNav()">&#9776; Menú</span>
+    <span id="btn-menu" class="btnMenu" onclick="openNav()">&#9776; Menú</span>
 </div> 
 <!-- Fin Botón Menú -->
 
